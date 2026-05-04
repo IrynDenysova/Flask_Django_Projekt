f74fe27418ca4463154243696c9ad5f0e373e215
IrynDenysova <iriska.ermak@gmail.com>
2026-05-04 18:04:54 +0200

    HW-6

--- community_pulse/app/__init__.py
+++ community_pulse/app/__init__.py
@@ -3,6 +3,7 @@ from flask import Flask
 from app.extentions import db, migrate
 from app.routers.questions import questions_bp
 from app.routers.response import response_bp
+from app.routers.category import categories_bp
 from config import DevelopmentConfig
 import app.models
 
@@ -14,4 +15,5 @@ def create_app():
     migrate.init_app(app, db)
     app.register_blueprint(questions_bp, url_prefix='/questions')
     app.register_blueprint(response_bp, url_prefix='/response')
+    app.register_blueprint(categories_bp, url_prefix='/categories')
     return app
--- community_pulse/app/schemas/question.py
+++ community_pulse/app/schemas/question.py
@@ -1,13 +1,20 @@
+from typing import Optional
+
 from pydantic import BaseModel, Field, ConfigDict
+
+from app.schemas.category import CategoryResponse
 
 
 class QuestionCreate(BaseModel):
     text: str = Field(..., min_length=12)
+    category_id: Optional[int] = None
 
 
 class QuestionResponse(BaseModel):
     id: int
     text: str
+    category: CategoryResponse | None = None
+    category_id: Optional[int] = None
 
     # class Config:
     #     # Указываем Pydantic использовать эти параметры чтобы можно было переносить данные прямо с объекта
